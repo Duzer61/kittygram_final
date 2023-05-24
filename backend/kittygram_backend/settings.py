dd9cb64334c459ceeb7cdb2ea31d390436b18f87
Данил Кочетов <dmi194@yandex.ru>
2023-05-24 13:20:15 +0300

    Локальные эксперименты

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -55,9 +55,9 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
+    'default': {    
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/data/db.sqlite3',
     }
 }
 
